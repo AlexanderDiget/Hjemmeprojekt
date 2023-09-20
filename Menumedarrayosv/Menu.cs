f44867faaac0f23b9ce21bdadde9243d074be5ac
Alexander Diget <alex40@live.dk>
2023-09-20 14:12:21 +0200

    test

---
+++ Menumedarrayosv/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menumedarrayosv
+{
+    public class Menu
+    {
+        public string Title;
+        private MenuItem[] menuItems;
+        private int itemCount = 0;
+        public Menu()
+        {
+            menuItems = new MenuItem[10];
+        }
+
+        public void Show()
+        {
+            Console.WriteLine(Title);
+            for (int i = 0; i < itemCount; i++)
+                Console.WriteLine(menuItems[i].Title);
+        }
+        public void AddmenuItem(string MenuTitle)
+        {
+            MenuItem mi = new MenuItem();
+            mi.Title = MenuTitle;
+            menuItems[itemCount] = mi;
+            itemCount++;
+        }
+    }
+}
+
---
+++ Menumedarrayosv/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Menumedarrayosv
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu mainMenu = new Menu();
+
+            mainMenu.Title = "Min fantastiske menu";
+
+            // First menu item
+            mainMenu.AddmenuItem("1. Gør dit");
+            // Increment with one; same as: ItemCount = ItemCount + 1
+
+            // Second menu item
+
+            mainMenu.AddmenuItem("2. Gør dat");
+
+            // Third menu item
+            mainMenu.AddmenuItem("3. Gør noget");
+
+            // Last menu item
+            mainMenu.AddmenuItem("4. Få svaret på livet, universet og alting");
+
+
+            mainMenu.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
