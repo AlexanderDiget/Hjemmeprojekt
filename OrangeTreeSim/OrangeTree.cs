c0757f1bd8c903a1025c63a8853e0a1919516fb4
Alexander Diget <alex40@live.dk>
2023-09-22 11:02:41 +0200

    opdatering OrangeTree

---
+++ OrangeTreeSim/OrangeTree.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeTreeSim
+{
+    public class OrangeTree
+    {
+        
+        private int age = 0;
+        private int height = 0;
+        private bool treeAlive = false;
+        private int numOranges = 0;
+        private int orangesEaten = 0;
+
+
+        public int Age
+        {
+            set 
+            {  
+                if (value >= 0)
+                { 
+                    age = value; 
+                }
+                else
+                {
+                    age = 0;
+                }
+            }
+            get { return age; } 
+        }
+        
+        public int Height
+        {
+            set { height = value; }
+            get { return height; }
+        }
+
+        public bool TreeAlive 
+        {
+            set {  treeAlive = value; }
+            get { return treeAlive; }
+        }
+
+        public int NumOranges
+        {
+            get { return numOranges; }
+        }
+
+        public int OrangesEaten
+        {
+            get { return orangesEaten; }
+        }
+
+        /*
+        public void SetAge(int age)
+        {
+            this.age = age;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+        public void SetHeight(int height)
+        {
+            this.height = height;
+        }
+        public int GetHeight()
+        {
+            return height;
+        }
+        public void SetTreeAlive(bool treeAlive)
+        {
+            this.treeAlive = treeAlive;
+        }
+        public bool GetTreeAlive()
+        {
+            return treeAlive;
+        }
+        public int GetNumOranges()
+        {
+            return numOranges;
+        }
+        public int GetOrangesEaten()
+        {
+            return orangesEaten;
+        }
+        */
+
+        public void OneYearPasses() 
+        {
+            age++;
+            numOranges = 0;
+            orangesEaten = 0;
+
+            if (age < 80)
+            {
+                TreeAlive = true;
+                height = height + 2;
+                numOranges = (age - 1 ) * 5;
+             /* Træets alder øges med 1 (age-feltet)
+                Træet højde øges med 2, hvis træets alder er under 80, ellers ikke (height-feltet)
+                Træet er i live, hvis træets alder er under 80, ellers dødt (treeAlive)
+                Træet giver ikke frugt første år, men derefter øges dets produktion med 5 appelsiner for hvert år (dvs. 5 appelsiner 2. år, 10 appelsiner det 3. år, 15 appelsiner det 4. år osv.). Frugt fra forrige år føres ikke med over til et nyt år (numOranges-feltet) 
+                 */
+            }
+            else 
+            {
+                TreeAlive = false;
+             
+            }
+            
+            // Første år (1-1)*5 = 0
+            // Andet år (2-1)*5 = 5
+            // Tredje år (3-1)*5 = 10
+        }
+        
+        public void EatOrange(int count) 
+        {
+            //EatOrange(count); Reccursion. Dette laver et uendeligt loop hvilket man ikke må
+            
+            if(numOranges >= count)
+            {
+                numOranges -= count;
+                orangesEaten += count;
+            }
+
+        }
+
+      
+    }
+}
---
+++ OrangeTreeTest/UnitTest1.cs
@@ -0,0 +1,159 @@
+using OrangeTreeSim;
+using System.IO.IsolatedStorage;
+
+namespace OrangeTreeTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private OrangeTree orangeTree;
+
+        [TestInitialize]
+        public void SetupForTest()
+        {
+            /*
+            // Start an orange tree
+            orangeTree = new OrangeTree();
+            orangeTree.SetAge(0);
+            orangeTree.SetHeight(6);
+            orangeTree.SetTreeAlive(true);
+            */
+            orangeTree = new OrangeTree();
+            orangeTree.Age = 0;
+            orangeTree.Height = 6;
+            orangeTree.TreeAlive = true;
+        }
+
+        [TestMethod]
+        public void ShouldIncrementTheTreesAgeWithEachPassingYear()
+        {
+            //Act
+            orangeTree.OneYearPasses();
+
+            //Assert
+            //Assert.AreEqual(1, orangeTree.GetAge());
+            Assert.AreEqual(1, orangeTree.Age);
+        }
+        [TestMethod]
+        public void ShouldIncrementTheTreesHeightByTwoWithEachPassingYear()
+        {
+            //Act
+            orangeTree.OneYearPasses();
+
+            //Assert
+            //Assert.AreEqual(8, orangeTree.GetHeight());
+            Assert.AreEqual(8, orangeTree.Height);
+        }
+        [TestMethod]
+        public void ShouldDieAfter80Years()
+        {
+            //Act
+            for (int i = 1; i <= 80; i++)
+            {
+                orangeTree.OneYearPasses();
+            }
+
+            //Assert
+            //Assert.AreEqual(false, orangeTree.GetTreeAlive());
+            Assert.AreEqual(false, orangeTree.TreeAlive);
+        }
+
+        [TestMethod]
+        public void ShouldProduceFruitAfter2Years()
+        {
+            orangeTree.OneYearPasses();
+            //Assert.AreEqual(0, orangeTree.GetNumOranges());
+            Assert.AreEqual(0, orangeTree.NumOranges);
+
+            orangeTree.OneYearPasses();
+            //Assert.AreEqual(5, orangeTree.GetNumOranges());
+            Assert.AreEqual(5, orangeTree.NumOranges);
+
+        }
+        [TestMethod]
+        public void ShouldIncreaseFruitProductionBy5PiecesEachYearAfterMaturity()
+        {
+            orangeTree.OneYearPasses();
+            orangeTree.OneYearPasses();
+            //Assert.AreEqual(5, orangeTree.GetNumOranges());
+            Assert.AreEqual(5, orangeTree.NumOranges);
+
+            orangeTree.OneYearPasses();
+            //Assert.AreEqual(10, orangeTree.GetNumOranges());
+            Assert.AreEqual(10, orangeTree.NumOranges);
+
+
+        }
+        [TestMethod]
+        public void ShouldCountNumberOfOrangesEatenThisYear()
+        {
+            orangeTree.OneYearPasses();
+            orangeTree.OneYearPasses();
+            orangeTree.EatOrange(1);
+
+            //Assert.AreEqual(1, orangeTree.GetOrangesEaten());
+            Assert.AreEqual(1, orangeTree.OrangesEaten);
+
+            orangeTree.EatOrange(3);
+            //Assert.AreEqual(4, orangeTree.GetOrangesEaten());
+            Assert.AreEqual(4, orangeTree.OrangesEaten);
+
+            //Assert.AreEqual(1, orangeTree.GetNumOranges());
+            Assert.AreEqual(1, orangeTree.NumOranges);
+        }
+        [TestMethod]
+        public void OrangesEatenOneYearPasses()
+        {
+            //Act
+            orangeTree.OneYearPasses();
+            orangeTree.OneYearPasses();
+            orangeTree.EatOrange(1);
+            orangeTree.EatOrange(3);
+            orangeTree.OneYearPasses();
+            //Assert
+            //Assert.AreEqual(0, orangeTree.GetOrangesEaten());
+            Assert.AreEqual(0, orangeTree.OrangesEaten);
+            //Assert.AreEqual(10, orangeTree.GetNumOranges());
+            Assert.AreEqual(10, orangeTree.NumOranges);
+        }
+
+        [TestMethod]
+        public void DeadTreeDoNotGrowAndProduceFruit()
+        {
+            //Act
+            for (int i = 1; i <= 80; i++)
+            {
+                orangeTree.OneYearPasses();
+            }
+
+            /*
+            Assert
+            Assert.AreEqual(false, orangeTree.GetTreeAlive());
+            Assert.AreEqual(0, orangeTree.GetNumOranges());
+            Assert.AreEqual(164, orangeTree.GetHeight());
+            Assert.AreEqual(81, orangeTree.GetAge());
+            Assert.AreEqual(0, orangeTree.GetNumOranges());
+            Assert.AreEqual(164, orangeTree.GetHeight());
+            Assert.AreEqual(82, orangeTree.GetAge());
+            */
+
+            Assert.AreEqual(false, orangeTree.TreeAlive);
+            orangeTree.OneYearPasses();
+            Assert.AreEqual(0, orangeTree.NumOranges);
+            Assert.AreEqual(164, orangeTree.Height);
+            Assert.AreEqual(81, orangeTree.Age);
+            orangeTree.OneYearPasses();
+            Assert.AreEqual(0, orangeTree.NumOranges);
+            Assert.AreEqual(164, orangeTree.Height);
+            Assert.AreEqual(82, orangeTree.Age);
+
+        }
+    }
+}
+
+    
+
+
+
+
+       
