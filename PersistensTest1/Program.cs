af98b29189785159c7c765a6e9b885cf2c12ae14
Alexander Diget <alex40@live.dk>
2023-09-29 10:21:07 +0200

    DataHandler, Arrays, kan bruges til budweg

--- PersistensTest1/Program.cs
+++ PersistensTest1/Program.cs
@@ -4,13 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Person person = new Person("Anders Andersen", new DateTime(1975, 8, 24), 175.9, true, 3);
+            const int names = 3;
+            Person[] persons = new Person[names];
+            persons[0] = new Person("Ulla Pedersen", new DateTime(1994, 3, 29), 186.5, false, 1);
+            persons[1] = new Person("Sully Pedersen", new DateTime(1995, 4, 30), 187, true, 2);
+            persons[2] = new Person("Henrik Plovmand", new DateTime(1965, 5, 31), 188, false, 3);
+
+            //Person person = new Person("Anders Andersen", new DateTime(1975, 8, 24), 175.9, true, 3);
 
             DataHandler handler = new DataHandler("Data.txt");
-            handler.SavePerson(person);
+            handler.SavePersons(persons);
 
-            Console.Write("Writing Person: ");
-            Console.WriteLine(person.MakeTitle());
+            //Console.Write("Writing Person: ");
+            //Console.WriteLine(person.MakeTitle());
 
             Console.ReadLine();
         }
