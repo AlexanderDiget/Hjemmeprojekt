5511a72413eb17616cbfecdb63418621d170f0c6
Alexander Diget <alex40@live.dk>
2023-11-09 11:20:41 +0100

    update

---
+++ Try.Finally StreamReader1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Try.Finally_StreamReader1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "test.txt";
+            StreamWriter SW = new StreamWriter(fileName);
+            SW.WriteLine("Hejsa");
+            SW.Close();
+            
+        }
+    }
+}
