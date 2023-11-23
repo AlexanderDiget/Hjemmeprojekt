965b89429b20da8356cc65b975f57595c5ac2528
Alexander Diget <alex40@live.dk>
2023-11-23 08:27:37 +0100

    WPFandMVVM2

---
+++ WPFandMVVM2/WPFandMVVM2/WPFAndMVVM2/ViewModels/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Configuration;
+using System.Security.Policy;
+using System.Text;
+using WPFAndMVVM2.Models;
+
+namespace WPFAndMVVM2.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private PersonRepository personRepo = new PersonRepository();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Implement the rest of this MainViewModel class below to 
+        // establish the foundation for data binding !
+        public ObservableCollection<PersonViewModel> PersonsVM {  get; set; } 
+
+        public MainViewModel()
+        {
+            PersonsVM = new ObservableCollection<PersonViewModel>(); //ObservableCollection kan se hvis man tilføjer noget til sin Collection
+            foreach (Person person in personRepo.GetAll())           //List kan ikke se hvis man tilføjer noget nyt til sin liste
+            {
+                PersonViewModel personViewModel = new PersonViewModel(person);
+                PersonsVM.Add(personViewModel);
+            } 
+            
+        }
+
+        private Person selectedPerson;
+
+        public Person SelectedPerson
+        {
+            get { return selectedPerson; }
+            set 
+            { 
+                selectedPerson = value;
+                OnPropertyChanged("SelectedPerson");
+
+            }
+        }
+        protected void OnPropertyChanged(string valgtPerson)
+        {
+            PropertyChangedEventHandler propertyChanged = this.PropertyChanged;
+            if (propertyChanged != null)
+            {
+                propertyChanged(this, new PropertyChangedEventArgs(valgtPerson));
+            }
+        }
+
+        public void AddDefaultPerson()
+        {
+            Person person = personRepo.Add("Specify FirstName", "Specify LastName", 0, "Specify Phone");
+            PersonViewModel personViewModel = new PersonViewModel(person);
+            PersonsVM.Add(personViewModel);
+        }
+        public void DeleteSelectedPerson() 
+        {
+            PersonViewModel personViewModel = new PersonViewModel(SelectedPerson);
+            PersonsVM.Remove(personViewModel);
+        }
+    }
+}
