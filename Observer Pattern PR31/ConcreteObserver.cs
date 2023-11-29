0ef887dd9676e5b839ab49cefdd163853b037f48
Alexander Diget <alex40@live.dk>
2023-11-29 13:03:54 +0100

    Observer Pattern

---
+++ Observer Pattern PR31/ConcreteObserver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer_Pattern_PR31
+{
+    public class ConcreteObserver : Observer
+    {
+        private ConcreteSubject subject;
+        private int state = 0;
+
+        public int State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public ConcreteObserver(ConcreteSubject subject)
+        {
+            this.subject = subject;
+        }
+        public override void Update()
+        {
+           State = subject.State;
+        }
+
+    }
+}
---
+++ Observer Pattern PR31/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer_Pattern_PR31
+{
+    public class Student : Observer
+    {
+        private Academy subject;
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value;}
+        }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+        }
+        public Student(Academy subject, string name)
+        {
+            this.subject = subject;
+            this.name = name;
+        }
+
+        public override void Update()
+        {
+            message = subject.Message;
+            Console.WriteLine("Studerende" + " " + name + " " + "modtog nyheden:" + " " + message + " " + "Fra akademiet" + " " + subject.Name);
+        }
+    }
+}
